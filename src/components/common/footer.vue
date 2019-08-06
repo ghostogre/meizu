1e02ad2cfc2a2650301b462fd42f06c26db3e9d2
yingjh <yjh.1234@qq.com>
2019-08-06 18:46:31 +0800

    footer

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="footer">
     <div class="footer-wrapper">
-      <div>
-        <ul>
+      <div class="clearfix">
+        <ul class="fl footer-service">
           <li v-for="(item, index) in serviceLinkData" :key="index">
             <a :href="item.href">
               <img :src="item.icon" alt="">
@@ -10,25 +10,27 @@
             </a>
           </li>
         </ul>
-        <div>
+        <div class="fr online-service">
           <span>周一至周五 7:30-24:00</span>
           <p>400-788-3333</p>
           <a href="">在线客服</a>
         </div>
       </div>
-      <hr>
-      <div>
-        <div>
-          <ul>
+      <hr class="footer-hr">
+      <div class="clearfix">
+        <div class="fl">
+          <ul class="external-link">
             <li v-for="(item, index) in externalLinkData" :key="index">
               <a :href="item.href">
                 {{item.name}}
               </a>
             </li>
           </ul>
-          <div></div>
+          <div class="footer-right">
+            2019@
+          </div>
         </div>
-        <div>
+        <div class="fr subscribe">
           <a href="">
             <i class="icon-font icon-weibo"></i>
           </a>
@@ -79,6 +81,85 @@ export default {
   &-wrapper {
     width: 1240px;
     margin: 0 auto;
+  }
+
+  &-service {
+    width: 1000px;
+    li {
+      display: inline-block;
+      width: 240px;
+      height: 57px;
+      font-size: 12px;
+      color: #999999;
+    }
+
+    img, span {
+      vertical-align: middle;
+    }
+  }
+
+  &-hr {
+    margin: 20px 0 28px 0;
+    display: block;
+    height: 1px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+
+  .online-service {
+    text-align: right;
+
+    span {
+      font-size: 10px;
+      color: #999;
+    }
+
+    p {
+      color: #00c3f5;
+      font-size: 30px;
+      font-weight: 400;
+    }
+  }
+
+  .external-link {
+    li {
+      display: inline-block;
+      padding-right: 15px;
+      margin-right: 15px;
+      border-right: 1px solid #d5d5d5;
+      font-size: 12px;
+      columns: #999;
+
+      &:last-child {
+        border: none;
+      }
+    }
+  }
+
+  &-right {
+    padding-top: 6px;
+    font-size: 12px;
+    color: #999;
+  }
+
+  
+  .subscribe {
+    margin-top: 18px;
+
+    a {
+      color: #999;
+      padding-right: 10px;
+      margin-right: 10px;
+      border-right: 1px solid #eee;
+
+      &:last-child {
+        border-right: none;
+      }
+
+      &:hover {
+        color: #31e5e7;
+      }
+    }
   }
 }
 
